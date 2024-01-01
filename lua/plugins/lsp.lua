9d541d02bb12d470330e5119b971961f2c3ea3d5
Nowaaru <nowaaru@proton.me>
2024-01-01 16:43:11 -0700

    stuff

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,47 @@
+return {
+  -- C# language server.
+  {
+    "razzmatazz/csharp-language-server",
+    enabled = true,
+  },
+  -- JSON language server.
+  {
+    "microsoft/vscode-json-languageservice",
+    enabled = true,
+  },
+  -- Lua language server.
+  {
+    "LuaLS/lua-language-server",
+    enabled = true,
+  },
+  -- Luau language server.
+  {
+    "JohnnyMorganz/luau-lsp",
+    enabled = true,
+  },
+  -- Typescript language server.
+  {
+    "typescript-language-server/typescript-language-server",
+    enabled = true,
+  },
+  -- Vim language server.
+  {
+    "iamcco/vim-language-server",
+    enabled = true,
+  },
+  -- Typescript language server.
+  {
+    "redhat-developer/yaml-language-server",
+    enabled = true,
+  },
+  -- CMake language server.
+  {
+    "Decodetalkers/neocmakelsp",
+    enabled = true,
+  },
+  -- Nix language server.
+  {
+    "oxalica/nil",
+    enabled = true,
+  },
+}
