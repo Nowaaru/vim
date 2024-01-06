f21ef7fd7178f6444a2ffa1e2ef43c1f0559cbae
Nowaaru <nowaaru@proton.me>
2024-01-05 19:21:54 -0700

    might be braindead
    oops

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,15 +14,15 @@ do
   vim.keymap.del("n", "<C-k>")
   vim.keymap.del("n", "<C-l>")
 
-  map("n", "<C-Up>", "<C-w>h", { desc = "Go to left window", remap = true })
-  map("n", "<C-Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
-  map("n", "<C-Left>", "<C-w>k", { desc = "Go to upper window", remap = true })
-  map("n", "<C-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
+  vim.keymap.set("n", "<C-Up>", "<C-w>h", { desc = "Go to left window", remap = true })
+  vim.keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
+  vim.keymap.set("n", "<C-Left>", "<C-w>k", { desc = "Go to upper window", remap = true })
+  vim.keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
 
   -- Resizing.
-  map("n", "<C-h>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-  map("n", "<C-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-  map("n", "<C-k>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-  map("n", "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+  vim.keymap.set("n", "<C-h>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+  vim.keymap.set("n", "<C-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+  vim.keymap.set("n", "<C-k>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+  vim.keymap.set("n", "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 end
 
