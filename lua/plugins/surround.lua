8c0ba028781c0731be673dda0d4e95f158c50cc3
Nowaaru <nowaaru@proton.me>
2024-01-05 19:16:07 -0700

    surround replace

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,17 @@
+-- Fix the surround bindings. Should be y(ou)s(urround)[args][motion]
+return {
+  {
+    "tpope/vim-surround",
+    enabled = true;
+  };
+
+  {
+    'wakatime/vim-wakatime',
+    enabled = true;
+  }
+
+  {
+    "echasnovski/mini.surround"
+    enabled = false;
+  };
+}
