65379489befab79998d540763c8cc055b34407b7
Nowaaru <nowaaru@proton.me>
2024-01-05 20:12:37 -0700

    test
    hmm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,10 +19,10 @@ do
   vim.keymap.del(n, "<C-k>")
   vim.keymap.del(n, "<C-l>")
 
-  vim.keymap.set(n, "<C-Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
-  vim.keymap.set(n, "<C-Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
-  vim.keymap.set(n, "<C-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
-  vim.keymap.set(n, "<C-Left>", "<C-w>h", { desc = "Go to left window", remap = true })
+  vim.keymap.set(n, "<C-Up>", "<C-w>k", { desc = "Go to upper window"})
+  vim.keymap.set(n, "<C-Down>", "<C-w>j", { desc = "Go to lower window"})
+  vim.keymap.set(n, "<C-Right>", "<C-w>l", { desc = "Go to right window"})
+  vim.keymap.set(n, "<C-Left>", "<C-w>h", { desc = "Go to left window"})
 
   -- Resizing.
   vim.keymap.set(n, "<C-k>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
