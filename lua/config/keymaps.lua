7855c732e17e1219018b4564c928b3df2b484ebc
Nowaaru <nowaaru@proton.me>
2024-01-05 19:56:35 -0700

    ???
    does it work?

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,25 +9,25 @@ local t = "t";
 local nt = { n, t };
 
 do
-  vim.keymap.del(nt, "<C-Up>")
-  vim.keymap.del(nt, "<C-Down>")
-  vim.keymap.del(nt, "<C-Left>")
-  vim.keymap.del(nt, "<C-Right>")
+  vim.keymap.del(n, "<C-Up>")
+  vim.keymap.del(n, "<C-Down>")
+  vim.keymap.del(n, "<C-Left>")
+  vim.keymap.del(n, "<C-Right>")
 
   vim.keymap.del(n, "<C-h>")
   vim.keymap.del(n, "<C-j>")
   vim.keymap.del(n, "<C-k>")
   vim.keymap.del(n, "<C-l>")
 
-  vim.keymap.set(nt, "<C-Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
-  vim.keymap.set(nt, "<C-Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
-  vim.keymap.set(nt, "<C-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
-  vim.keymap.set(nt, "<C-Left>", "<C-w>h", { desc = "Go to left window", remap = true })
+  vim.keymap.set(n, "<C-Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
+  vim.keymap.set(n, "<C-Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
+  vim.keymap.set(n, "<C-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
+  vim.keymap.set(n, "<C-Left>", "<C-w>h", { desc = "Go to left window", remap = true })
 
   -- Resizing.
-  vim.keymap.set(nt, "<C-k>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-  vim.keymap.set(nt, "<C-j>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-  vim.keymap.set(nt, "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
-  vim.keymap.set(nt, "<C-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+  vim.keymap.set(n, "<C-k>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+  vim.keymap.set(n, "<C-j>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+  vim.keymap.set(n, "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+  vim.keymap.set(n, "<C-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 end
 
