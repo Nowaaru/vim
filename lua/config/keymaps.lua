241b6df2c41e0ebe6e63ef9bef98d674b671ecd5
Nowaaru <nowaaru@proton.me>
2024-01-05 19:46:16 -0700

    hoping it sticks
    (it wont)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,26 +3,28 @@
 -- Add any additional keymaps here
 
 -- Swap window size and window change mappings.
+
+local nt = { "n", "t" }
 do
-  vim.keymap.del("n", "<C-Up>")
-  vim.keymap.del("n", "<C-Down>")
-  vim.keymap.del("n", "<C-Left>")
-  vim.keymap.del("n", "<C-Right>")
+  vim.keymap.del(nt, "t"}, "<C-Up>")
+  vim.keymap.del(nt, "<C-Down>")
+  vim.keymap.del(nt, "<C-Left>")
+  vim.keymap.del(nt, "<C-Right>")
 
-  vim.keymap.del({ "n", "t" }, "<C-h>")
-  vim.keymap.del({ "n", "t" }, "<C-j>")
-  vim.keymap.del({ "n", "t" }, "<C-k>")
-  vim.keymap.del({ "n", "t" }, "<C-l>")
+  vim.keymap.del(nt, "<C-h>")
+  vim.keymap.del(nt, "<C-j>")
+  vim.keymap.del(nt, "<C-k>")
+  vim.keymap.del(nt, "<C-l>")
 
-  vim.keymap.set({ "n", "t" }, "<C-Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
-  vim.keymap.set({ "n", "t" }, "<C-Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
-  vim.keymap.set({ "n", "t" }, "<C-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
-  vim.keymap.set({ "n", "t" }, "<C-Left>", "<C-w>h", { desc = "Go to left window", remap = true })
+  vim.keymap.set(nt, "<C-Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
+  vim.keymap.set(nt, "<C-Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
+  vim.keymap.set(nt, "<C-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
+  vim.keymap.set(nt, "<C-Left>", "<C-w>h", { desc = "Go to left window", remap = true })
 
   -- Resizing.
-  vim.keymap.set({"n", "t"}, "<C-k>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-  vim.keymap.set({"n", "t"}, "<C-j>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-  vim.keymap.set({"n", "t"}, "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
-  vim.keymap.set({"n", "t"}, "<C-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+  vim.keymap.set(nt, "<C-k>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+  vim.keymap.set(nt, "<C-j>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+  vim.keymap.set(nt, "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+  vim.keymap.set(nt, "<C-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 end
 
